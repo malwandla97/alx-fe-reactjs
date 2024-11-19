d61cdeda96bcba833f282057d777866f841a03ef
malwandla97 <mandlas703@gmail.com>
2024-11-19 14:58:32 +0200

    Add files via upload

---
+++ recipe-sharing-app/src/components/recipedetails.js
@@ -0,0 +1,27 @@
+// RecipeDetails.js
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
+import { useNavigate } from 'react-router-dom';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore(state =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
+  );
+  
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
