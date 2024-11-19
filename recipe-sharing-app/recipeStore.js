e361e433ee931cd7edef7f364443428837106bbe
malwandla97 <mandlas703@gmail.com>
2024-11-19 13:33:08 +0200

    Add files via upload

--- recipe-sharing-app/recipeStore.js
+++ recipe-sharing-app/recipeStore.js
@@ -1,9 +1,28 @@
+// recipeStore.js
+
 import create from 'zustand';
 
-const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-}));
+export const useRecipeStore = create((set) => ({
+  recipes: [
+    // Sample recipes
+    { id: 1, title: 'Spaghetti Bolognese', description: 'A classic Italian pasta dish with meat sauce.' },
+    { id: 2, title: 'Chicken Curry', description: 'A spicy and flavorful chicken curry.' }
+  ],
+  
+  // Action to add a new recipe
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe]
+  })),
 
-export { useRecipeStore };
+  // Action to update an existing recipe
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+
+  // Action to delete a recipe
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id)
+  }))
+}));
