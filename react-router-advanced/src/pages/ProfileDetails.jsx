4425e47573e30e37b03c0fe6c57436243d3f72e1
malwandla97 <mandlas703@gmail.com>
2024-11-20 11:14:12 +0200

    Add files via upload

---
+++ react-router-advanced/src/pages/ProfileDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfileDetails = () => {
+  return (
+    <div>
+      <h3>Profile Details</h3>
+      <p>Some details about the user...</p>
+    </div>
+  );
+};
+
+export default ProfileDetails;
