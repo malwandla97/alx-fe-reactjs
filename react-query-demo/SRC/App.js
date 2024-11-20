ba99a2f853a4daed546bcbb68c778f5bb79921c1
malwandla97 <mandlas703@gmail.com>
2024-11-20 10:45:57 +0200

    Add files via upload

--- react-query-demo/SRC/App.js
+++ react-query-demo/SRC/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
 
+// Initialize the QueryClient
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
