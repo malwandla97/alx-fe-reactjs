c1ce75760964ff2ae198c1fbc99744edd0681c12
malwandla97 <mandlas703@gmail.com>
2024-11-20 11:11:36 +0200

    Add files via upload

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Post from './pages/Post';
+import NotFound from './pages/NotFound';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="post/:id" element={<Post />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
