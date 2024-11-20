120b86cf44667fef5a8d3e4ff76bcf39763a8dd1
malwandla97 <mandlas703@gmail.com>
2024-11-20 11:20:38 +0200

    Add files via upload

--- react-router-advanced/App.jsx
+++ react-router-advanced/App.jsx
@@ -1,3 +1,6 @@
+<Route path="post/:id" element={<Post />} />
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
