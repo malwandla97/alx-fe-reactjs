5815677f2c033b3bfd331063e46990dbebcfd69e
malwandla97 <mandlas703@gmail.com>
2024-12-07 09:05:56 +0200

    Add files via upload

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm"
+
+const App = () => {
+  return(
+    <div>
+      <h1>User Registration</h1>
+      <RegistrationForm />
+      <FormikForm />
+    </div>
+  );
+};
+export default App;
