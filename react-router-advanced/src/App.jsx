d7c4d4050867a4514c6b0c2c62c911ca3bc652f3
malwandla97 <mandlas703@gmail.com>
2024-12-07 09:28:51 +0200

    Add files via upload

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Post from './pages/Post';
-import NotFound from './pages/NotFound';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+const Home = () => <h1>Home Page</h1>;
+const About = () => <h1>About Page</h1>;
+
+const App = () => {
+  const isAuthenticated = false; // Simulating authentication state
+
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/1">Sample Blog Post</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="post/:id" element={<Post />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+
+        {/* Protected Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
