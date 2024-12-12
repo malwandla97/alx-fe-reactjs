6f24d8478aca1cc5d82eec3e5a040b171a692fb3
malwandla97 <mandlas703@gmail.com>
2024-12-12 15:41:46 +0200

    Add files via upload

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,7 +1,8 @@
+// tailwind.config.js
 module.exports = {
     content: [
       './index.html',
-      './src/**/*.{js,jsx,ts,tsx}',
+      './src/**/*.{js,jsx,ts,tsx}',  // Add this line to make sure Tailwind checks React components
     ],
     theme: {
       extend: {},
