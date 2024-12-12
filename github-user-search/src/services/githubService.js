1649659436ed24b420cb54da70fbefab23edf38d
malwandla97 <mandlas703@gmail.com>
2024-12-12 15:16:00 +0200

    Add files via upload

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,13 @@
+
+import axios from 'axios';
+
+const GITHUB_API_URL = 'https://api.github.com/users/';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}${username}`);
+    return response.data;  // Return the user data
+  } catch (error) {
+    throw new Error('User not found');  // Throw an error if something goes wrong
+  }
+};
