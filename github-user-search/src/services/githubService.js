7e7ac546163decf8fac7dd80d04b4532ab1e070b
malwandla97 <mandlas703@gmail.com>
2024-12-13 08:07:56 +0200

    Add files via upload

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,34 @@
 
 import axios from 'axios';
 
-const GITHUB_SEARCH_URL = 'https://api.github.com/search/users?q=';
+const API_URL = 'https://api.github.com/search/users';
 
-export const fetchUserData = async (query) => {
+const fetchUserData = async (username, location = '', minRepos = 0) => {
   try {
-    const response = await axios.get(`${GITHUB_SEARCH_URL}${query}`);
-    return response.data; // Return the response with search results
+    let query = `q=${username}`;
+
+    // If location is provided, add it to the query
+    if (location) {
+      query += `+location:${location}`;
+    }
+
+    // If minRepos is provided, add it to the query
+    if (minRepos > 0) {
+      query += `+repos:>=${minRepos}`;
+    }
+
+    // Fetch data from GitHub API with the constructed query
+    const response = await axios.get(`${API_URL}?${query}`);
+
+    // Return the data of users
+    return response.data.items;
   } catch (error) {
-    throw new Error('Error fetching data from GitHub');
+    // Return null in case of an error
+    console.error("Error fetching data from GitHub API:", error);
+    return null;
   }
+};
+
+export default {
+  fetchUserData,
 };
